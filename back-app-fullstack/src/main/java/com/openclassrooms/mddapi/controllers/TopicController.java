bb41ff6d2b2e6867f0a64f4e8d8b3d3f3e7eddfa
alexia-pratensi <alexiapratensi@hotmail.fr>
2023-10-24 20:32:11 +0200

    added controllers

---
+++ back-app-fullstack/src/main/java/com/openclassrooms/mddapi/controllers/TopicController.java
@@ -0,0 +1,52 @@
+package com.openclassrooms.mddapi.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.openclassrooms.mddapi.dto.TopicDto;
+import com.openclassrooms.mddapi.mappers.TopicMapper;
+import com.openclassrooms.mddapi.models.Topic;
+import com.openclassrooms.mddapi.repositories.TopicRepository;
+import com.openclassrooms.mddapi.services.TopicService;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/topics")
+@CrossOrigin(origins = "*", maxAge = 3600)
+public class TopicController {
+
+    @Autowired
+    private TopicService topicService;
+
+    // @Autowired
+    // private TopicMapper topicMapper;
+
+    @GetMapping
+    public ResponseEntity<?> getAllTopics() {
+        try {
+            List<Topic> topics = topicService.getAllTopics();
+            // List<TopicDto> topicDtos = topicMapper.toDto(topics);
+            return ResponseEntity.ok().body(topics);
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().body("Error: " + e.getMessage());
+        }
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<?> findTopicById(@PathVariable Long id) { // Utilisez @PathVariable pour obtenir l'ID depuis
+                                                                    // l'URL
+        try {
+            Topic topic = topicService.findById(id);
+            // TopicDto topicDto = topicMapper.toDto(topic);
+            return ResponseEntity.ok().body(topic);
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().body("Error: " + e.getMessage());
+        }
+    }
+}
